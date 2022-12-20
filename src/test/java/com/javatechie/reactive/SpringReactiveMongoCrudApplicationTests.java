c948b65eb0d29cbfbfe4a82304f516f41b884dcd
ajit51 <ajitk433@gmail.com>
2022-12-21 00:04:34 +0530

    Spring Boot WebFlux | Write Integration Test Using Mockito & Junit

--- src/test/java/com/javatechie/reactive/SpringReactiveMongoCrudApplicationTests.java
+++ src/test/java/com/javatechie/reactive/SpringReactiveMongoCrudApplicationTests.java
@@ -1,13 +1,103 @@
 package com.javatechie.reactive;
 
+import com.javatechie.reactive.controller.ProductController;
+import com.javatechie.reactive.dto.ProductDto;
+import com.javatechie.reactive.service.ProductService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(ProductController.class)
 class SpringReactiveMongoCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
 
+    @MockBean
+    private ProductService service;
+
+    @Test
+    public void addProductTest() {
+        Mono<ProductDto> productDtoMono = Mono.just(new ProductDto("102", "Mobile", 1, 10000));
+        when(service.saveProduct(productDtoMono)).thenReturn(productDtoMono);
+
+        webTestClient.post().uri("/products")
+                .body(Mono.just(productDtoMono), ProductDto.class)
+                .exchange()
+                .expectStatus().isOk();//200
+    }
+
+    @Test
+    public void getProductsTest() {
+        Flux<ProductDto> productDtoFlux = Flux.just(
+                new ProductDto("102", "Mobile", 1, 10000),
+                new ProductDto("103", "TV", 1, 50000)
+        );
+
+        when(service.getProducts()).thenReturn(productDtoFlux);
+
+        Flux<ProductDto> responseBody = webTestClient.get().uri("/products")
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(ProductDto.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBody)
+                .expectSubscription()
+                .expectNext(new ProductDto("102", "Mobile", 1, 10000))
+                .expectNext(new ProductDto("103", "TV", 1, 50000))
+                .verifyComplete();
+    }
+
+    @Test
+    public void getProductTest() {
+        Mono<ProductDto> productDtoMono = Mono.just(new ProductDto("102", "Mobile", 1, 10000));
+
+        when(service.getProduct(any())).thenReturn(productDtoMono);
+
+        Flux<ProductDto> responseBody = webTestClient.get().uri("/products/102")
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(ProductDto.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBody)
+                .expectSubscription()
+                .expectNextMatches(p -> p.getName().equals("Mobile"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void updateProduct() {
+        Mono<ProductDto> productDtoMono = Mono.just(new ProductDto("102", "Mobile", 1, 10000));
+
+        when(service.updateProduct(productDtoMono, "102")).thenReturn(productDtoMono);
+
+        webTestClient.put().uri("/products/update/102")
+                .body(Mono.just(productDtoMono), ProductDto.class)
+                .exchange()
+                .expectStatus().isOk();//200
+    }
+
+
+    @Test
+    public void deleteProduct() {
+        given(service.deleteCustomer(any())).willReturn(Mono.empty());
+
+        webTestClient.delete().uri("/products/delete/102")
+                .exchange()
+                .expectStatus().isOk();
+    }
 }
